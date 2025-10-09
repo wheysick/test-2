6a53531f3bea1b81438017947c725f36bf430df9
wheysick <wheysick@gmail.com>
2025-10-08 17:17:38 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
   - Cash App (Square):  squareAppId, squareLocationId  (needs server endpoint)
 */
 window.CO_CONFIG = {
-  recurlyPublicKey: "",            // e.g. "ewr1-xxxxxx"
+  recurlyPublicKey: "ewr1-AxqCz2aZ9UMj5oOBsENPG2",            // e.g. "ewr1-xxxxxx"
   paypalClientId:   "",            // e.g. "Abc123..."
   coinbaseCheckoutId: "",          // e.g. "a1b2c3d4e5f6..."
   squareAppId: "",                 // e.g. "sandbox-sq0idb-..."
