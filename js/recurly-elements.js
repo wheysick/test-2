e54c05c7cc86746a7837bf45b51fb642feddc2aa
wheysick <wheysick@gmail.com>
2025-10-09 16:16:56 -0700

    Update recurly-elements.js

--- js/recurly-elements.js
+++ js/recurly-elements.js
@@ -85,8 +85,9 @@ const elements = recurly.Elements({
   style: {
     fontFamily: 'Inter, system-ui, -apple-system, Segoe UI, Arial, sans-serif',
     fontSize: '16px',
-    color: '#e9ecf2',                          // typed text color
-    placeholder: { color: 'rgba(255,255,255,.55)' },
-    invalid:     { color: '#ff9ab3' }
+    color: '#e9ecf2',                                 // typed text color
+    placeholder: { color: 'rgba(255,255,255,.55)' },  // placeholder color
+    invalid:     { color: '#ff9ab3' }                 // invalid color
   }
 });
+
